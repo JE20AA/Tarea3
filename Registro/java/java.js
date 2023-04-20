cc5989daee77c02d8415ea323176cd6421b00d31
JE20AA <106342717+JE20AA@users.noreply.github.com>
2023-04-20 00:09:45 -0500

    Add files via upload

---
+++ Registro/java/java.js
@@ -0,0 +1,14 @@
+alert("Bienvenidos al registro");
+alert("Ingrese los datos que se pide, por favor");
+
+
+var inputs = document.getElementsByClassName('formulario');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function() {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
